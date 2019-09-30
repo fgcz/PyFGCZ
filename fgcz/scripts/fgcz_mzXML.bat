2c3394305c99bf62a13cb352025f9d88d4126131
Christian Panse <cpanse@users.noreply.github.com>
2019-09-30 08:15:55 +0200

    Update fgcz_mzXML.bat

--- fgcz/scripts/fgcz_mzXML.bat
+++ fgcz/scripts/fgcz_mzXML.bat
@@ -1,1 +1,6 @@
+rem fcc converter script for generating mzXML using msconvert
+rem Christian Panse <cp@fgcz.ethz.ch> 2019-09-27
+
 "C:\Program Files (x86)\ProteoWizard\ProteoWizard 3.0.10200\msconvert" %1 --mzXML -o %~dp1\msconvert_mzXML
+
+rem see also https://stackoverflow.com/questions/659647/how-to-get-folder-path-from-file-path-with-cmd/659672
