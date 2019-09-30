0dc36f522427caaaf0170f51a1681391e86efe2f
Christian Panse <cpanse@users.noreply.github.com>
2019-09-30 08:16:28 +0200

    Update fgcz_mzXML.bat

--- fgcz/scripts/fgcz_mzXML.bat
+++ fgcz/scripts/fgcz_mzXML.bat
@@ -1,5 +1,6 @@
 rem fcc converter script for generating mzXML using msconvert
 rem Christian Panse <cp@fgcz.ethz.ch> 2019-09-27
+rem tested on fgcz-r-026.uzh.ch
 
 "C:\Program Files (x86)\ProteoWizard\ProteoWizard 3.0.10200\msconvert" %1 --mzXML -o %~dp1\msconvert_mzXML
 
